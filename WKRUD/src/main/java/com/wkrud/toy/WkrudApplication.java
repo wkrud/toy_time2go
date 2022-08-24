bea10b3eb634e27906ede405ea53f63027523a37
wkrud <jae6140@naver.com>
2022-08-24 21:18:08 +0900

    rr

---
+++ WKRUD/src/main/java/com/wkrud/toy/WkrudApplication.java
@@ -0,0 +1,13 @@
+package com.wkrud.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkrudApplication.class, args);
+	}
+
+}
---
+++ WKRUD/src/test/java/com/wkrud/toy/WkrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wkrud.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WkrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
